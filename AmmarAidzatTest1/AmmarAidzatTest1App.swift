9bd5e953e1b81eeae34418fdade920d582b9e296
ammar <ammaraidzat98@gmail.com>
2023-03-22 13:38:21 +0800

    Initial Commit

---
+++ AmmarAidzatTest1/AmmarAidzatTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  AmmarAidzatTest1App.swift
+//  AmmarAidzatTest1
+//
+//  Created by Ammar Aidzat on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AmmarAidzatTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
